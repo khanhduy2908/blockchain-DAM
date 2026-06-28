b1ce8e9d9b8a2c334caee67ef7b3ca2177f4949b
Duy Ngo <ngohoangkhanhduy.work@gmail.com>
2026-06-28 22:19:41 +0700

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+    log(network.name)
+    log("----------------------------------------------------")
+    const assetPrice = "10000000000000000" // 0.01 ETH
+
+    const arguments = [assetPrice]
+    const digitalAssetContract = await deploy("DigitalAssetContract", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(digitalAssetContract.address, arguments)
+    }
+    log("----------------------------------------------------")
+}
+
+module.exports.tags = ["deploy"]
