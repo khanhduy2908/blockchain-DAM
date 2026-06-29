18b98e62559ddd5eecd0c77b72284395caf06ee9
Duy Ngo <ngohoangkhanhduy.work@gmail.com>
2026-06-29 15:25:20 +0000

    Fix Hardhat config and local deploy script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,31 +1,24 @@
 const { network } = require("hardhat")
-const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
-const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    const waitBlockConfirmations = developmentChains.includes(network.name)
-        ? 1
-        : VERIFICATION_BLOCK_CONFIRMATIONS
-    log(network.name)
-    log("----------------------------------------------------")
-    const assetPrice = "10000000000000000" // 0.01 ETH
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
 
-    const arguments = [assetPrice]
-    const digitalAssetContract = await deploy("DigitalAssetContract", {
-        from: deployer,
-        args: arguments,
-        log: true,
-        waitConfirmations: waitBlockConfirmations,
-    })
+  const listingFee = "10000000000000000" // 0.01 ETH
 
-    // Verify the deployment
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying...")
-        await verify(digitalAssetContract.address, arguments)
-    }
-    log("----------------------------------------------------")
+  log("----------------------------------------------------")
+  log(`Deploying to network: ${network.name}`)
+  log(`Deployer: ${deployer}`)
+
+  const marketplace = await deploy("DamMartketplace", {
+    from: deployer,
+    args: [listingFee],
+    log: true,
+    waitConfirmations: 1,
+  })
+
+  log(`DamMartketplace deployed at: ${marketplace.address}`)
+  log("----------------------------------------------------")
 }
 
-module.exports.tags = ["deploy"]
+module.exports.tags = ["marketplace"]
